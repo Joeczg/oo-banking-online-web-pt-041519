ca58fb02e8952363ab827ff8b392f50e36c4ed84
zhenggao cai <caizhengao1992@gmail.com>
2019-05-26 03:14:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,12 @@
 class Transfer
   # your code here
+  attr_accessor :name,:account,:amount,:bank_account
+  def initialize(name,account,amount)
+    @name = name 
+    @account = account
+    @amount = amount
+  end
+  def sender 
+   BankAccount.new(self.name)
+end
 end
